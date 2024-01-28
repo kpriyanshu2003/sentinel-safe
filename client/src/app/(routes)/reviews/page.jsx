a850260958309feba316a1c5b0757f9810c0bc5b
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-01-29 01:29:18 +0530

    Implemented Firebase Authentication functionality

---
+++ client/src/app/(routes)/reviews/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>aaaaaaa</div>;
+};
+
+export default page;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div style={{ backgroundColor: "#AAA9BC" }}>
+      <div className="bg-gray-200 ">
         <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
           <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
